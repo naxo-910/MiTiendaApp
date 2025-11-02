e613e6e75aa4c249c8ff3a468b65087943117d5f
naxo-910 <xxbackxxi@gmail.com>
2025-11-02 18:53:10 -0300

    Fase 1: Mejorada validación de formato de email y largo de contraseña

--- app/src/main/java/cl/mitienda/app/MainActivity.kt
+++ app/src/main/java/cl/mitienda/app/MainActivity.kt
@@ -3,6 +3,7 @@ package cl.mitienda.app
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import cl.mitienda.app.databinding.ActivityMainBinding
+import android.util.Patterns
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,23 +15,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // --- LÓGICA DE VALIDACIÓN MEJORADA (IE 2.2.1) ---
         binding.buttonLogin.setOnClickListener {
 
             val email = binding.editTextEmail.text.toString().trim()
             val password = binding.editTextPassword.text.toString().trim()
 
+            // Reseteamos errores antiguos
             binding.editTextEmail.error = null
             binding.editTextPassword.error = null
 
+            // --- NUEVA LÓGICA DE VALIDACIÓN ---
+
+            // 1. Revisamos si el email está vacío
             if (email.isEmpty()) {
                 binding.editTextEmail.error = "Este campo es obligatorio"
             }
+            // 2. NUEVO: Revisamos si el email NO tiene formato válido
+            else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.editTextEmail.error = "Formato de correo inválido (ej: usuario@dominio.com)"
+            }
+            // 3. Revisamos si la contraseña está vacía
             else if (password.isEmpty()) {
                 binding.editTextPassword.error = "Este campo es obligatorio"
             }
+            // 4. NUEVO: Revisamos si la contraseña es muy corta
+            else if (password.length < 6) {
+                binding.editTextPassword.error = "La contraseña debe tener al menos 6 caracteres"
+            }
 
             else {
-                // Lógica de éxito (la añadiremos después)
+                // ¡ÉXITO! Todos los campos son válidos.
+                // Por ahora, solo mostramos un mensaje temporal.
+                // (Añadiremos esto en el siguiente paso)
             }
         }
     }
