fd6468b5d7e3d3c0b652d542268247082ea44122
naxo-910 <xxbackxxi@gmail.com>
2025-11-02 18:41:31 -0300

    Fase 1: Implementada validación de campos vacíos en Login

--- app/src/main/java/cl/mitienda/app/MainActivity.kt
+++ app/src/main/java/cl/mitienda/app/MainActivity.kt
@@ -1,20 +1,37 @@
 package cl.mitienda.app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import cl.mitienda.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonLogin.setOnClickListener {
+
+            val email = binding.editTextEmail.text.toString().trim()
+            val password = binding.editTextPassword.text.toString().trim()
+
+            binding.editTextEmail.error = null
+            binding.editTextPassword.error = null
+
+            if (email.isEmpty()) {
+                binding.editTextEmail.error = "Este campo es obligatorio"
+            }
+            else if (password.isEmpty()) {
+                binding.editTextPassword.error = "Este campo es obligatorio"
+            }
+
+            else {
+                // Lógica de éxito (la añadiremos después)
+            }
         }
     }
 }
